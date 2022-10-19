f08c99d247e4017595414a31bbd30ccf8f27fee1
Георгий Воякин <voyaking@gmail.com>
2022-10-19 23:39:11 +0300

    Update Server

--- pkss-pr2/src/pkss/pr2/Server.java
+++ pkss-pr2/src/pkss/pr2/Server.java
@@ -1,4 +1,33 @@
 package pkss.pr2;
 
-public class Server {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server extends Thread {
+    private ServerSocket serverSocket;
+    private final int port = 1001;
+    private BufferedReader fromClient;
+    private PrintStream toClient;
+
+    public Server() {
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            System.err.println("Не удалось создать серверный сокет с портом " + port + " : " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                Socket client = serverSocket.accept();
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
