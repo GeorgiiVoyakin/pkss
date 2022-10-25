a414647146b9cbc082c94decdaa9a4b1e06559d5
Георгий Воякин <voyaking@gmail.com>
2022-10-25 20:47:42 +0300

    Add separate main methods for server and client

---
+++ pkss-pr2/src/pkss/pr2/ClientMain.java
@@ -0,0 +1,9 @@
+package pkss.pr2;
+
+import java.io.IOException;
+
+public class ClientMain {
+    public static void main(String[] args) throws IOException {
+        new Client().start();
+    }
+}
