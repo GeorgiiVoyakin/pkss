d138aedd0b2413a25ce0c6bef9859068151c18ea
Георгий Воякин <voyaking@gmail.com>
2022-10-25 21:54:57 +0300

    Update Client and Server to perform all given tasks

--- pkss-pr2/src/pkss/pr2/Client.java
+++ pkss-pr2/src/pkss/pr2/Client.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Client extends Thread {
     private final BufferedReader fromServer;
@@ -19,9 +21,67 @@ public class Client extends Thread {
     @Override
     @SuppressWarnings("InfiniteLoopStatement")
     public void run() {
-        toServer.println("Hello");
         try {
             while (true) {
+                System.out.println("Client> Выберите операцию: [5, 8, 11, 14, 17]");
+                Scanner scanner = new Scanner(System.in);
+                int choice;
+                try {
+                    choice = scanner.nextInt();
+                } catch (InputMismatchException e) {
+                    System.out.println("Client> Введите число. Доступные операции: [5, 8, 11, 14, 17]");
+                    continue;
+                }
+                switch (choice) {
+                    case 5 -> {
+                        toServer.println(choice);
+                        System.out.println("Client> Введите 2 числа:");
+                        double S = scanner.nextDouble();
+                        double T = scanner.nextDouble();
+                        toServer.println(S);
+                        toServer.println(T);
+                    }
+                    case 8 -> {
+                        toServer.println(choice);
+                        System.out.println("Client> Введите координаты шестиугольника:");
+                        StringBuilder stringBuilder = new StringBuilder();
+                        for (int i = 0; i < 12; i++) {
+                            stringBuilder.append(scanner.nextInt());
+                            stringBuilder.append(" ");
+                        }
+                        toServer.println(stringBuilder);
+                    }
+                    case 11 -> {
+                        toServer.println(choice);
+                        System.out.println("Client> Введите 3 числа:");
+                        StringBuilder stringBuilder = new StringBuilder();
+                        stringBuilder.append(scanner.nextInt());
+                        stringBuilder.append(" ");
+                        stringBuilder.append(scanner.nextInt());
+                        stringBuilder.append(" ");
+                        stringBuilder.append(scanner.nextInt());
+                        toServer.println(stringBuilder);
+                    }
+                    case 14 -> {
+                        toServer.println(choice);
+                        System.out.println("Client> Введите 1 число:");
+                        toServer.println(scanner.nextDouble());
+                    }
+                    case 17 -> {
+                        toServer.println(choice);
+                        System.out.println("Client> Введите координаты восьмиугольника:");
+                        StringBuilder stringBuilder = new StringBuilder();
+                        for (int i = 0; i < 16; i++) {
+                            stringBuilder.append(scanner.nextInt());
+                            stringBuilder.append(" ");
+                        }
+                        toServer.println(stringBuilder);
+                    }
+                    default -> {
+                        System.out.println("Client> Выбрана не допустимая операция. Доступные операции: [5, 8, 11, 14, 17]");
+                        continue;
+                    }
+                }
                 String message = fromServer.readLine();
                 if (message != null) {
                     System.out.println("Client> Получено сообщение от сервера: " + message);
--- pkss-pr2/src/pkss/pr2/Server.java
+++ pkss-pr2/src/pkss/pr2/Server.java
@@ -7,6 +7,8 @@ import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import static java.util.Arrays.*;
+
 public class Server extends Thread {
     private final ServerSocket serverSocket;
     private final int port = 1001;
@@ -54,7 +56,42 @@ public class Server extends Thread {
                     String message = fromClient.readLine();
                     if (message != null) {
                         System.out.println("Server> Получено сообщение от клиента: " + message);
-                        toClient.println("Сообщение успешно получено");
+                        int choice;
+                        try {
+                            choice = Integer.parseInt(message);
+                        } catch (NumberFormatException e) {
+                            toClient.println("Введите число. Доступные операции: [5, 8, 11, 14, 17]");
+                            continue;
+                        }
+                        switch (choice) {
+                            case 5 -> {
+                                toClient.println(Solution.task5(Double.parseDouble(fromClient.readLine()), Double.parseDouble(fromClient.readLine())));
+                            }
+                            case 8 -> {
+                                int[] coords = stream(fromClient.readLine().split("\s"))
+                                        .mapToInt(Integer::parseInt)
+                                        .toArray();
+                                toClient.println(Solution.task8(coords));
+                            }
+                            case 11 -> {
+                                int[] nums = stream(fromClient.readLine().split("\s"))
+                                        .mapToInt(Integer::parseInt)
+                                        .toArray();
+                                toClient.println(Solution.task11(nums[0], nums[1], nums[2]));
+                            }
+                            case 14 -> {
+                                toClient.println(Solution.task14(Double.parseDouble(fromClient.readLine())));
+                            }
+                            case 17 -> {
+                                int[] coords = stream(fromClient.readLine().split("\s"))
+                                        .mapToInt(Integer::parseInt)
+                                        .toArray();
+                                toClient.println(Solution.task17(coords));
+                            }
+                            default -> {
+                                toClient.println("Выбранная не доступная операция. Доступные операции: [5, 8, 11, 14, 17]");
+                            }
+                        }
                     }
                 }
             } catch (IOException e) {
