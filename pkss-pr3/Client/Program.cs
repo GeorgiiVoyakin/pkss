e8f90d022a99e313b2a03a8f31def7211e108a3d
GeorgiiVoyakin <voyaking@gmail.com>
2022-11-03 13:28:11 +0300

    Update practice 3

--- pkss-pr3/Client/Program.cs
+++ pkss-pr3/Client/Program.cs
@@ -11,7 +11,108 @@ namespace Client
             StreamReader reader = new StreamReader(client.GetStream());
             StreamWriter writer = new StreamWriter(client.GetStream());
 
-            Console.WriteLine("Client> Получено сообщение от сервера: " + reader.ReadLine());
+            while (true)
+            {
+                try
+                {
+                    bool skip = false;
+                    Console.WriteLine("Client> Выберите операцию: [5, 8, 11, 14, 17]:");
+                    int choice = Int32.Parse(Console.ReadLine()!);
+                    switch (choice)
+                    {
+                        case 5:
+                            Console.WriteLine("Client> Введите N: ");
+                            int N;
+                            try
+                            {
+                                N = Int32.Parse(Console.ReadLine()!);
+                            }
+                            catch (FormatException)
+                            {
+                                Console.WriteLine("Client> Некорректный ввод!");
+                                continue;
+                            }
+
+                            writer.WriteLine(choice);
+                            writer.WriteLine(N);
+                            writer.Flush();
+                            break;
+                        case 8:
+                            Console.WriteLine("Client> Введите A: ");
+                            double A;
+                            try
+                            {
+                                A = Double.Parse(Console.ReadLine()!);
+                            }
+                            catch (FormatException)
+                            {
+                                Console.WriteLine("Client> Некорректный ввод!");
+                                continue;
+                            }
+                            writer.WriteLine(choice);
+                            writer.WriteLine(A);
+                            writer.Flush();
+                            break;
+                        case 11:
+                            Console.WriteLine("Client> Введите A: ");
+                            try
+                            {
+                                A = Double.Parse(Console.ReadLine()!);
+                            }
+                            catch (FormatException)
+                            {
+                                Console.WriteLine("Client> Некорректный ввод!");
+                                continue;
+                            }
+                            writer.WriteLine(choice);
+                            writer.WriteLine(A);
+                            writer.Flush();
+                            break;
+                        case 14:
+                            writer.WriteLine(choice);
+                            writer.Flush();
+                            break;
+                        case 17:
+                            writer.WriteLine(choice);
+                            writer.Flush();
+                            String response;
+                            do
+                            {
+                                Console.WriteLine("Client> Введите число: ");
+                                try
+                                {
+                                    N = Int32.Parse(Console.ReadLine()!);
+                                }
+                                catch (FormatException)
+                                {
+                                    Console.WriteLine("Client> Некорректный ввод!");
+                                    writer.WriteLine(0);
+                                    writer.Flush();
+                                    reader.ReadLine();
+                                    break;
+                                }
+                                writer.WriteLine(N);
+                                writer.Flush();
+                                response = reader.ReadLine()!;
+                            } while (Boolean.Parse(response));
+                            skip = true;
+                            break;
+                        default:
+                            writer.WriteLine(choice);
+                            writer.Flush();
+                            break;
+                    }
+                    String? message = reader.ReadLine();
+                    if (message is not null && !skip)
+                    {
+                        Console.WriteLine("Client> Получено сообщение от сервера: " + message);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Client> Выбрана не допустимая операция");
+                }
+            }
         }
     }
 }
--- pkss-pr3/Server/Program.cs
+++ pkss-pr3/Server/Program.cs
@@ -27,7 +27,90 @@ namespace Server
             StreamReader reader = new StreamReader((tcpClient as TcpClient)!.GetStream());
             StreamWriter writer = new StreamWriter((tcpClient as TcpClient)!.GetStream());
             writer.AutoFlush = true;
-            writer.WriteLine("Hello");
+
+            while (true)
+            {
+                String? message = reader.ReadLine();
+                if (message is null)
+                {
+                    continue;
+                }
+                int choice = Int32.Parse(message);
+                Console.WriteLine("Server> Получено сообщение от клиента, выбрана операция: " + choice);
+                switch (choice)
+                {
+                    case 5:
+                        int N = Int32.Parse(reader.ReadLine()!);
+                        writer.WriteLine(Solution.task5(N));
+                        break;
+                    case 8:
+                        double A = Double.Parse(reader.ReadLine()!);
+                        writer.WriteLine(String.Join(", ", Solution.task8(A).ToArray()));
+                        break;
+                    case 11:
+                        A = Double.Parse(reader.ReadLine()!);
+                        writer.WriteLine(String.Join(", ", Solution.task11(A).ToArray()));
+                        break;
+                    case 14:
+                        writer.WriteLine(String.Join(", ", Solution.task14().ToArray()));
+                        break;
+                    case 17:
+                        N = Int32.Parse(reader.ReadLine()!);
+                        while (!(N == 0 || N % 2 == 0))
+                        {
+                            writer.WriteLine(true);
+                            N = Int32.Parse(reader.ReadLine()!);
+                        }
+                        writer.WriteLine(false);
+                        writer.WriteLine("");
+                        break;
+                    default:
+                        writer.WriteLine("Выбранная не доступная операция. Доступные операции: [5, 8, 11, 14, 17]");
+                        break;
+                }
+            }
+        }
+    }
+
+    class Solution
+    {
+        public static bool task5(int N)
+        {
+            while (N % 5 == 0)
+            {
+                N /= 5;
+            }
+            return N == 1;
+        }
+
+        public static List<double> task8(double A)
+        {
+            List<double> result = new List<double>();
+            for (double x = -2; x < 8; x += 0.75)
+            {
+                result.Add((4 * x - 3 * x + Math.Tan(x)) / A);
+            }
+            return result;
+        }
+
+        public static List<double> task11(double A)
+        {
+            List<double> result = new List<double>();
+            for (double x = 2; x < 8; x += 0.75)
+            {
+                result.Add((4 * x - 3 * x + Math.Cos(x)) / A);
+            }
+            return result;
+        }
+
+        public static List<double> task14()
+        {
+            List<double> result = new List<double>();
+            for (double x = -3; x <= 11; x += 0.9)
+            {
+                result.Add(Math.Cos(x) - 5 * Math.Sin(x - 2));
+            }
+            return result;
         }
     }
 }
