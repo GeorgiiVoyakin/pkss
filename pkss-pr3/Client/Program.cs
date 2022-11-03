a61cf29f88bd88d085f4a604ec462327eded89dc
GeorgiiVoyakin <voyaking@gmail.com>
2022-11-03 10:34:35 +0300

    Add working example of client server

--- pkss-pr3/Client/Program.cs
+++ pkss-pr3/Client/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Client
+﻿using System.Net.Sockets;
+
+namespace Client
 {
     class Program
     {
         static void Main()
         {
-            Console.WriteLine("Client");
+            int Port = 1002;
+            TcpClient client = new TcpClient("localhost", Port);
+            StreamReader reader = new StreamReader(client.GetStream());
+            StreamWriter writer = new StreamWriter(client.GetStream());
+
+            Console.WriteLine("Client> Получено сообщение от сервера: " + reader.ReadLine());
         }
     }
 }
--- pkss-pr3/Server/Program.cs
+++ pkss-pr3/Server/Program.cs
@@ -8,9 +8,26 @@ namespace Server
         static void Main()
         {
             int Port = 1002;
-            TcpListener listener = new TcpListener(new IPEndPoint(IPAddress.Parse("localhost"), Port));
+            TcpListener listener = new TcpListener(new IPEndPoint(IPAddress.Parse("127.0.0.1"), Port));
             listener.Start();
             Console.WriteLine("Server> Сервер запущен на порту: " + Port);
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("Server> Установлено соединение с клиентом: " + client.Client.RemoteEndPoint);
+
+                Thread t = new Thread(new ParameterizedThreadStart(ClientHandler));
+                t.Start(client);
+            }
+        }
+
+        static void ClientHandler(object? tcpClient)
+        {
+            StreamReader reader = new StreamReader((tcpClient as TcpClient)!.GetStream());
+            StreamWriter writer = new StreamWriter((tcpClient as TcpClient)!.GetStream());
+            writer.AutoFlush = true;
+            writer.WriteLine("Hello");
         }
     }
 }
